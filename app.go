bce19915f85c21a9d0c21b3cbcbe918449a0e571
Zidane Arkan <zidark897@gmail.com>
2024-07-04 21:59:34 +0700

    Add env for test

--- app.go
+++ app.go
@@ -4,6 +4,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
@@ -15,7 +18,17 @@ type App struct {
 
 // The Initialize method will take in the details required to connect to the database.
 // It will create a database connection and wire up the routes to respond according to the requirements.
-func (a *App) Initialize(user, password, dbname string) {}
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+}
 
 // The Run method will simply start the application.
 func (a *App) Run(addr string) {}
